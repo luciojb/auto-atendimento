a22d911957e37e4bbe71a96209cb9ea58ec702af
lucio <lucio@Luciojb>
2016-11-24 12:53:58 -0200

    Updating

---
+++ Auto Atendimento/src/teste.java
@@ -0,0 +1,30 @@
+import java.util.List;
+import DAO.CartaoDAO;
+import DTO.CartaoDTO;
+import generic.GenericDAO;
+
+
+
+public class teste {
+	public static void main(String[] args){
+		CartaoDTO c = new CartaoDTO();
+		c.setAgencia(54546);
+		c.setBandeira("Visa");
+		c.setNumeroCartao(12341234000L);
+		c.setNumeroContaBancaria(66737);
+		c.setSaldo(1000);
+		c.setSenha(123456);
+		
+		CartaoDAO cd = new CartaoDAO();
+		
+		if (cd instanceof GenericDAO<?>){
+			cd.insert(c);
+			List<CartaoDTO> cList = cd.findAll();
+			for(Object ct : cList){
+				System.out.println(((CartaoDTO) ct).getNumeroContaBancaria());
+			}
+		}
+			
+			
+	}
+}
